6341ffc5bfef0f1de07c2b4391940ce8dce04155
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2019-12-16 23:48:31 +0530

    Update header.php

--- header.php
+++ header.php
@@ -7,46 +7,87 @@
     width: 100%;
     z-index: 5050;
     background-color: #fafafb;
-    transition: box-shadow cubic-bezier(.165, .84, .44, 1).25s;
+    transition: box-shadow 0px 8px 16px 0px rgba(0,0,0,0.2);
     height: 50px;
     box-sizing: border-box;
     font-family: Arial,"Helvetica Neue",Helvetica,sans-serif;
-    border-top: 3px solid #f48024;
+    border-top: 3px solid #1DB954;
     border-bottom: 1px solid lightgrey;
 }
-
-.sidenav {
-  height: 100%;
-  width: 130px;
-  position: fixed;
-  z-index: 1;
-  top: 40px;
-  left:0;
-  background: #fafafb;
-  overflow-x: hidden;
-  padding: 8px 0;
+.dropbtn {
+  background-color: #833AB4;
+  color: white;
+  padding: 11px;
+  font-size: 11px;
+  border: none;
+  cursor: pointer;
+  font-weight: light;
+        font-family: 'Trebuchet MS', sans-serif;
+        border: none;
+        border-radius: 5%;
 }
 
-.sidenav a {
-  padding: 6px 8px 6px 16px;
+.dropbtn:hover, .dropbtn:focus {
+  background-color: #DB4437;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  overflow: auto;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
   text-decoration: none;
-  font-size: 25px;
-  color: #f48024;
   display: block;
 }
 
-.sidenav a:hover {
-  color: lightgrey;
-}
+.dropdown a:hover {background-color: #ddd;}
+
+.show {display: block;}
 -->
 </style>
-<div class="top-bar">GNE GATE FORUM</div>
-<div class="sidenav">
-  <a href="#about">About</a>
-  <a href="#services">Services</a>
-  <a href="#clients">Clients</a>
-  <a href="#contact">Contact</a>
+<div class="top-bar">GNE GATE FORUM
+<div style="position: absolute; top:8%;left:90%;"class="dropdown">
+  <button onclick="myFunction()" class="dropbtn">SignIn/SignUp</button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="index.php">SignIn</a>
+    <a href="signup.php">SignUp</a>
+  </div>
 </div>
+</div>
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
   <?php @$_SESSION['login']; 
   error_reporting(1);
   ?>
