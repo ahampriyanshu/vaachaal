c9e0bac18c420924c8b348484465477ea53d0151
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2019-12-17 04:25:49 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
 <title>GATE Forum</title>
 <link href="forum.css" rel="stylesheet" type="text/css">
 </head>
-<body style="top: 20%;">
+<body background="back.jpg">
 <?php
 include("header.php");
 include("database.php");
@@ -26,39 +26,19 @@ if(isset($submit))
 	}
 }
 if (isset($_SESSION[login]))
-{echo '<table width="28%"  border="0" align="center">
-		
-  <tr>
-    <td width="7%" height="65" valign="bottom"></td>
-    <td width="93%" valign="bottom" bordercolor="#0000FF"> <a href="showtest.php?subid=1" class="style4" >Start Your Attempt</a></td>
-  </tr>
-</table>';
-   
+{
 		exit;
-		
-
 }
-
-
 ?>
+<div class="signinbox" style="position: absolute; top:16%;right:5%;">
 <table width="100%" border="0">
-  
-  
-    		<table align="center" border="0" WIDTH="50%" height="250">
-			<h1 class="text-center bg-warning">LOGIN PAGE</h1>
-		<form method="post" action="">
-		<center>
-		<img class="img-circle" src="1.jpg"  title="this is my profile pic" width="210px" height="200px" border="1" />
-		</center>	<br>
-	
-				<tr>
-					<th class="text-primary">LOGIN ID</th>
-					<th>
-					<input class="form-control"type="TEXT" title="enter your regitered LOGIN ID"  placeholder="LOGIN ID"  maxlength="10" size="25"  id="loginid2" name="loginid"/></tD>
+<table align="center" border="0" WIDTH="50%" height="250">
+<form method="post" action="">
+<center><img class="logocircle" src="1.png"  title="logo" width="210px" height="200px" border="1" /></center><tr><th>
+					<input class="login_text_box"type="TEXT" title="enter your regitered LOGIN ID"  placeholder="LOGIN ID"  maxlength="10" size="25"  id="loginid2" name="loginid"/></td>
 				</th>
 				<tr>
-					<th class="text-primary">ENTER PASSWORD</th>
-					<th><input class="form-control" type="password"  placeholder="ENTER PASSWORD" name="pass" id="pass2"/></th>
+					<th><input class="login_text_box" type="password"  placeholder="ENTER PASSWORD" name="pass" id="pass2"/></th>
 					</tr>
 					       <?php
 		  if(isset($found))
@@ -67,17 +47,15 @@ if (isset($_SESSION[login]))
 		  }
 		  ?>
           </span></td>
-         <th></th>
-				<th class="errors">
-					<input class="btn btn-danger "type="submit" name="submit" id="submit" Value="Login"/>
+       				<th class="errors">
+					<input class="submit" type="submit" name="submit" id="submit" Value="Login"/>
 				
-        <a class="btn btn-success" href="signup.php">Click here to Register</a></th>
+        <a class="submit2" href="signup.php">New User?</a></th>
       </table>
-      <div align="center">
-        </div>
     </form></td>
   </tr>
 </table>
+</div>
 
 </body>
 </html>
