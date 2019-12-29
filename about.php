783288342be5f6967a0b363e5f84431b9784f362
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2019-12-29 23:58:47 +0530

    Update about.php

--- about.php
+++ about.php
@@ -28,14 +28,14 @@
 <?php
 include("header.php");
 
-if(isset($_SESSION['login'])){
-  exit;
-    echo "<div class=\"abt\" style=\"position: absolute; top:12%;left:20%;\">
+if (isset($_SESSION['login']))
+{
+echo "<div class=\"abt\" style=\"position: absolute; top:12%;left:20%;\">
   <img class=\"img-circle\" style=\"position: absolute; top:1%;left:5%;\" src=\"IIS.jpg\"  width=\"200px\" height=\"150px\" />
     </center>
-    <img class=\"img-circle\" style=\"position: absolute; top:3%;left:35%;\" src=\"IITD.png\"  width=\"180px\" height=\"120px\" />
+    <img class=\"img-circle\" style=\"position: absolute; top:3%;left:38%;\" src=\"IITD.png\"  width=\"170px\" height=\"130px\" />
     </center>
-    <img class=\"img-circle\" style=\"position: absolute; top:3%;right:8%;\" src=\"IITM.png\"  width=\"180px\" height=\"120px\" />
+    <img class=\"img-circle\" style=\"position: absolute; top:3%;right:8%;\" src=\"IITM.png\"  width=\"160px\" height=\"130px\" />
     </center>
   <h1 style=\"position:absolute;top:26%;left:10%;\" class=\"heading\"><center>GRADUATE APTITUDE TEST IN ENGINEERING</center></h1>
   <h style=\"position:absolute;top:38%;left: 3%;right: 3%;\" class=\"sub\">GATE is a computer-based exam conducted at the national level with an aim to examine the understanding of various Engineering and Science UG subjects.GATE exam consists of 65 MCQs and numerical question over a 3 hour duration.GATE 2020 is being conducted by IIT-D,IIT-M IISc.GATE score is valid for 3 years and enables students to gain admission to various PG programs such as ME,BE and PhD in IITs,IISc and several other prestigious universities.Top rank holders also get direct interview calls for prestigious government jobs in PSUs</h>
@@ -46,18 +46,14 @@ else {
   echo "<div class=\"abt\" style=\"position: absolute; top:12%;left:5%;\">
   <img class=\"img-circle\" style=\"position: absolute; top:1%;left:5%;\" src=\"IIS.jpg\"  width=\"200px\" height=\"150px\" />
     </center>
-    <img class=\"img-circle\" style=\"position: absolute; top:3%;left:35%;\" src=\"IITD.png\"  width=\"180px\" height=\"120px\" />
+    <img class=\"img-circle\" style=\"position: absolute; top:3%;left:38%;\" src=\"IITD.png\"  width=\"170px\" height=\"130px\" />
     </center>
-    <img class=\"img-circle\" style=\"position: absolute; top:3%;right:8%;\" src=\"IITM.png\"  width=\"180px\" height=\"120px\" />
+    <img class=\"img-circle\" style=\"position: absolute; top:3%;right:8%;\" src=\"IITM.png\"  width=\"160px\" height=\"130px\" />
     </center>
   <h1 style=\"position:absolute;top:26%;left:10%;\" class=\"heading\"><center>GRADUATE APTITUDE TEST IN ENGINEERING</center></h1>
   <h style=\"position:absolute;top:38%;left: 3%;right: 3%;\" class=\"sub\">GATE is a computer-based exam conducted at the national level with an aim to examine the understanding of various Engineering and Science UG subjects.GATE exam consists of 65 MCQs and numerical question over a 3 hour duration.GATE 2020 is being conducted by IIT-D,IIT-M IISc.GATE score is valid for 3 years and enables students to gain admission to various PG programs such as ME,BE and PhD in IITs,IISc and several other prestigious universities.Top rank holders also get direct interview calls for prestigious government jobs in PSUs</h>
   <img class=\"img-circle\" style=\"position: absolute; bottom:2%;left:5%;\" src=\"PSU.png\"  width=\"750px\" height=\"220px\" />
 </div>";
-
 }
 ?>
-</script>
-  <?php @$_SESSION['login']; 
-  error_reporting(1);
-  ?>  
+</script> 
