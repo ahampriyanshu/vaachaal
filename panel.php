97a1d18c506997c2df8a928c1d71b5bcd9f269a6
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2019-12-30 22:28:38 +0530

    Add files via upload

---
+++ panel.php
@@ -0,0 +1,27 @@
+<?php
+include("database.php");
+?>
+<style type="text/css">
+
+<!--
+.panel{
+    position: absolute;
+    top: 8%;
+    left: 0;
+    width: 17%;
+    z-index: 5050;
+    background-color: #fafafb;
+    transition: box-shadow 0px 8px 16px 0px rgba(0,0,0,0.2);
+    height: 100%;
+    box-sizing: border-box;
+    font-family: Arial,"Helvetica Neue",Helvetica,sans-serif;
+    border-right: 2px solid lightgrey;
+
+}
+
+-->
+</style>   
+<div class="panel">
+
+</div> 
+
