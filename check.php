a1663366eaf2dd19afcbb249082c6739122cf041
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-06 14:41:55 +0530

    Create check.php

---
+++ check.php
@@ -0,0 +1,27 @@
+<?php include("essentials/database.php"); ?>
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+global $username;
+
+$sql = "INSERT INTO userbase (username,password)
+VALUES ('$username', '$password')";
+
+$q = "select * from userbase where login = '$username' && pass = '$password'";
+
+$result = mysqli_query($con,$q);
+
+$num = mysqli_num_rows($result);
+
+if ($num == 1) {
+	header('location:session.php');
+} 
+else {
+    $found="N";
+    global $found;
+    echo "<script>
+document.location='index.php';
+</script>";
+}
+?>
