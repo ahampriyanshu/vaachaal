64af1496c19afa8e24da504607291bea2bbb1259
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-06 14:44:34 +0530

    Update session.php

--- session.php
+++ session.php
@@ -1,26 +1,8 @@
-<?php include("header1.php"); ?>
+<?php session_start(); ?>
 <?php include("essentials/database.php"); ?>
-<?php include("about.php"); ?>
 <?php // include("essentials/function.php"); ?>
 <?php
-extract($_POST);
+		$_SESSION['user']=$username;
+		header('location:add.php');
 
-if(isset($submit))
-{
-	$rs=mysqli_query($con,"select * from mst_user where login='$username' and pass='$password'");
-	if(mysqli_num_rows($rs)<1)
-	{
-		$found="N";
-	}
-	else
-	{
-		$_SESSION['login']=$username;
-	}
-}
-
-if (isset($_SESSION['login']))
-{
-	echo "<script>location.href='add.php'</script>";
-	exit;
-}
 ?>
