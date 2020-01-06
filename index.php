92e6c7a5d98388b6e4b4156b25e0f4bf02473cdd
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-06 14:41:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
-<?php include("header1.php"); ?>
+<?php include("header.php"); ?>
 <?php include("essentials/database.php"); ?>
 <?php include("about.php"); ?>
 <?php // include("essentials/function.php"); ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 <style>
 th, td {
-padding: 5px;
+
   }
 </style>
 <title>LogIn</title>
@@ -35,11 +34,10 @@ padding: 5px;
 <body background="img/back.jpg">				
   <div class="signinbox" style="position: absolute; top:16%;right:5%;">
     <table align="center" border="0" WIDTH="90%" height="250">
-	 <form method="post" name="login_form" action="session.php" onSubmit="return check();">
+	 <form method="post" name="login_form" action="check.php" onSubmit="return check();">
 	<center><img class="logocircle" src="img/1.png"  title="logo" width="210px" height="200px" border="1" /></center>
 	<tr>
-	<th><input class="login_text_box" type="TEXT" title="enter your regitered LOGIN ID"  placeholder="LOGIN ID"  maxlength="10"
-    size="25"  id="loginid2" name="username"/></th>
+	<th><input class="login_text_box" type="TEXT" title="enter your regitered LOGIN ID"  placeholder="LOGIN ID"  maxlength="10" size="25"  id="loginid2" name="username"/></th>
 	</tr>
 	<tr> 	
     <th><input class="login_text_box" type="password"  placeholder="ENTER PASSWORD" name="password" id="pass2"/></th>
@@ -56,7 +54,7 @@ padding: 5px;
     	<button class="submit2" onclick="window.location.href = 'signup.php';" >New User?</button>
     </td>
 	</tr>
-   </form>
   </div>
+  </form>
 </body>
 </html>
