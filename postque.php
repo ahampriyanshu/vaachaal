839918bb2de532cb3cfb8323e375356d3ad08308
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-07 20:31:28 +0530

    Update postque.php

--- postque.php
+++ postque.php
@@ -1,9 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<?php  session_start(); ?>
+<?php
+include 'essentials/database.php';
+$title = $_POST['title'];
+$content = $_POST['content'];
+$level = $_POST['level'];
+$tym = $_POST['tym'];
+$branch = $_POST['branch'];
+$username = $_SESSION["loggedin"];
+$date = date('m/d/Y h:i:s', time());
 
-</body>
-</html>
+$sql = "INSERT INTO questions(title,content,level,tym,branch,username,datetym) VALUES ('$title','$content','$level','$tym','$branch','$username','$date')";
+
+if ($con->query($sql) === TRUE) {
+    header('location:viewque.php');
+} else {
+	header('location:add.php');
+    echo "<script>alert('SORRY,some error occured!Please try again');</script>";
+}	
