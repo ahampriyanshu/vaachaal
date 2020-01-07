09a18ebd16e7e0b0e022b80e00118f1021cabda2
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-07 20:28:25 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,63 +5,14 @@
 <script language="javascript">
 function check()
 {
-
- if(document.form1.lid.value=="")
-  {
-    alert("Plese Enter Login Id");
-	document.form1.lid.focus();
-	return false;
-  }
- 
- if(document.form1.pass.value=="")
-  {
-    alert("Plese Enter Your Password");
-	document.form1.pass.focus();
-	return false;
-  } 
-  if(document.form1.cpass.value=="")
-  {
-    alert("Plese Enter Confirm Password");
-	document.form1.cpass.focus();
-	return false;
-  }
-  if(document.form1.pass.value!=document.form1.cpass.value)
+  if(document.signupform.pass.value!=document.signupform.cpass.value)
   {
     alert("Confirm Password does not matched");
-	document.form1.cpass.focus();
+	document.signupform.cpass.focus();
 	return false;
   }
-  if(document.form1.name.value=="")
-  {
-    alert("Plese Enter Your Name");
-	document.form1.name.focus();
-	return false;
-  }
-  if(document.form1.name.value=="")
-  {
-    alert("Plese Enter name");
-	document.form1.name.focus();
-	return false;
-  }
-  if(document.form1.security.value=="")
-  {
-    alert("Plese Enter security Name");
-	document.form1.security.focus();
-	return false;
-  }
-  if(document.form1.phone.value=="")
-  {
-    alert("Plese Enter Contact No");
-	document.form1.phone.focus();
-	return false;
-  }
-  if(document.form1.email.value=="")
-  {
-    alert("Plese Enter your Email ID");
-	document.form1.email.focus();
-	return false;
-  }
-  e=document.form1.email.value;
+
+  e=document.signupform.email.value;
 		f1=e.indexOf('@');
 		f2=e.indexOf('@',f1+1);
 		e1=e.indexOf('.');
@@ -71,7 +22,7 @@ function check()
 		if(!(f1>0 && f2==-1 && e1>0 && e2==-1 && f1!=e1+1 && e1!=f1+1 && f1!=n-1 && e1!=n-1))
 		{
 			alert("Please Enter valid Email");
-			document.form1.email.focus();
+			document.signupform.email.focus();
 			return false;
 		}
   return true;
@@ -88,41 +39,38 @@ include("header.php");
   <center>
   <img class="img-circle" src="img/1.png"  width="200px" height="150px" style="position: relative; top:0%;"/>
   </center>
- <table width="100%" border="0">
+ <center><table width="80%" border="0">
   <tr><td>
-  <form name="form1" method="post" action="signupuser.php" onSubmit="return check();">
-	<center>
-    <table>
-		  <tr><td><input class="login_text_box" type="text" placeholder="Create Username" name="username" ></td>
-         </tr>
-         <tr>
-           <td><input class="login_text_box" type="password" placeholder="Create Password" name="pass"></td>
-         </tr>
-         <tr>
-           <td><input class="login_text_box" name="cpass" placeholder="Confirm Password" type="password" id="cpass"></td>
-         </tr>
-         <tr>
-           <td><input class="login_text_box" type="text" name="name" placeholder="Full name" id="name"></textarea></td>
-         </tr>
-         <tr>
-           <td><input class="login_text_box" name="security" type="text" id="security"
-            placeholder="Name of your childhood freind"></td>
-         </tr>
-         <tr>
-           <td><input class="login_text_box" name="phone" placeholder="+91" type="text" id="phone"></td>
-         </tr>
-         <tr>
-           <td><input class="login_text_box" name="email" type="text" placeholder="Enter valid e-mail" id="email"></td>
-         </tr>
-         <td>&emsp;
-          <input class="submit" type="submit" name="Submit" value="Signup">&emsp;&emsp;&emsp;
-		   <button class="submit2" onclick="window.location.href = 'index.php';" >Already a Member</button>
-    </td>   
-       </table>
-     </form>
-   </tr>
- </table>
+  <form name="signupform" method="post" action="signupuser.php" onSubmit="return check();">
+	    <tr>
+        <td><input class="login_text_box" type="text" placeholder="Create Username" name="username"  required>
+        </td>
+      </tr>
+      <tr>
+        <td><input class="login_text_box" type="password" placeholder="Create Password" name="pass" required></td>
+      </tr>
+      <tr>
+         <td><input class="login_text_box" name="cpass" placeholder="Confirm Password" type="password" id="cpass" required></td>
+      </tr>
+      <tr>
+         <td><input class="login_text_box" type="text" name="name" placeholder="Full name" id="name" required></td>
+      </tr>
+      <tr>
+         <td><input class="login_text_box" name="security" type="text" id="security"
+            placeholder="Name of your childhood freind" required></td>
+      </tr>
+      <tr>
+         <td><input class="login_text_box" name="phone" placeholder="+91" type="text" id="phone" required></td>
+      </tr>
+      <tr>
+         <td><input class="login_text_box" name="email" type="text" placeholder="Enter valid e-mail" id="email" required></td>
+      </tr>
+        <td>&emsp;<input class="submit" type="submit" name="Submit" value="Signup">&emsp;&emsp;</form>
+		    <button class="submit2" onclick="window.location.href = 'index.php';" >Already a Member</button></td>   
+      </tr>
+      </table>
 </center>
+&emsp;&emsp;
 </div>
 </body>
 </html>
