4a8bd2ae1fe4cecb3644254e6508872e9a9274ee
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-08 20:30:23 +0530

    Update postque.php

--- postque.php
+++ postque.php
@@ -1,6 +1,8 @@
 <?php  session_start(); ?>
 <?php
+
 include 'essentials/database.php';
+
 $title = $_POST['title'];
 $content = $_POST['content'];
 $level = $_POST['level'];
@@ -11,9 +13,12 @@ $date = date('m/d/Y h:i:s', time());
 
 $sql = "INSERT INTO questions(title,content,level,tym,branch,username,datetym) VALUES ('$title','$content','$level','$tym','$branch','$username','$date')";
 
-if ($con->query($sql) === TRUE) {
-    header('location:viewque.php');
-} else {
+if ($con->query($sql) === TRUE) 
+{
+    header('location:home.php');
+} 
+else 
+{
 	header('location:add.php');
     echo "<script>alert('SORRY,some error occured!Please try again');</script>";
 }	
