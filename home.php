934f9fd9a3829ef2feef81b75febe7d6a838f972
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-08 23:35:36 +0530

    Update home.php

--- home.php
+++ home.php
@@ -22,21 +22,22 @@ body{
     border: 2px solid #e2e2e2;
     box-shadow: 0 0 5px #888;
     border-radius: 5px;
-    width: 70%;
+    width: 100%;
     position: relative;
     bottom: initial;
     margin: auto;
     overflow-y: hidden;
 }
 .collapsible {
-  background-color: #777;
-  color: white;
+  background-color: white;
+  color: black;
   cursor: pointer;
-  padding: 18px;
   width: 100%;
-  border: none;
+  border: 1px solid #e2e2e2;
+  box-shadow: 0 0 5px #888;
+  border-radius: 4px;
   text-align: left;
-  outline: none;
+  outline: ;
   font-size: 15px;
 }
 
@@ -50,67 +51,49 @@ body{
   overflow: hidden;
   background-color: #f1f1f1;
 }
+
 </style>
 </head>
 <body >
-<?php
+    <?php
 $sql = "SELECT id,title,content,level,tym,branch,username,datetym FROM questions";
 $result = $con->query($sql);
 
 if ($result->num_rows > 0) 
- while($row = $result->fetch_assoc())  :?>
-  
-<button type="button" class="collapsible">
-  <form method="POST" action="addans.php"><br>
-   <b><center><?php echo $row["title"]; ?></center></b><br>
+        while($row = $result->fetch_assoc()) :?>
+                <br><br>
+        <div class="collapsible" >
+            <form method="post" action="addans.php">
+              <br>
+                <b><center><?php echo $row["title"]; ?></center></b><br>
                  <?php echo $row["username"]; ?> 
                  <?php echo $row["level"]; ?> 
                  <?php echo $row["tym"]; ?> 
                  <?php echo $row["branch"]; ?> 
                  <?php echo $row["datetym"]; ?><br><br>
                  <?php echo $row["content"]; ?>
-<input type="hidden" name="id" value="<?php $row['id']; ?>"/><br><br>
-<input type="submit" class="submit2" id="submit2" value="answer" /></form></button>
-<div class="content">
-  <p>
-     <?php
-        $qid = $row["id"];
-        $answersql = "SELECT * FROM answers WHERE id = '$qid'";
-        $answerresult = $con -> query ($answersql) ;
+                <input type="hidden" name="id" value="<?php echo $row['id']; ?>"/><br>
+                <button type="submit" class="submit2" >ANSWER</button><br><br>
+            </form>
+        </div>
+        <?php
+        $queid = $row["id"];
+        $ql = "SELECT * FROM answers WHERE id = '$queid'";
+$resul = $con->query($ql);
 
-if ($answerresult -> num_rows > 0) 
-{
-    while($ro = $answerresult->fetch_assoc())
-  {
-      echo "<div class=\"bg-danger tabcontent\" style=\"border: 5px solid red; padding: 10px; margin-left: 15px;\">";
+if ($resul->num_rows > 0) {
+    while($ro = $resul->fetch_assoc()) {
+        echo "<div  style=\"padding: 10px; \">";
         echo "<form>";
-        echo "<br><b> Answer id:</b> ". $ro["id"] . "<br><b> Answer:</b> ". $ro["content"] . "<br><b> Time required(mins):</b> " . $ro ["tym"] . "<br><b> Difficlty Level:</b> ". $ro["level"] ."<br><b> Answered by:</b> " . $ro["username"] . "<br><b> Email:</b> " . $ro["datetym"] ."<br>" ;
+        echo "<br><b> Solution </b> ". $ro["content"] . "<br><b> Time taken by the user </b> " . $ro ["tym"] . "<br><b> Difficlty Level according to user:</b> ". $ro["level"] ."<br><b> Answered by:</b> " . $ro["username"] . "<br><b> answered on :</b> " . $ro["datetym"] ."<br>" ;
         echo "<br></form></div>";
-    }}
-else
-{
-    echo '<div class="bg-danger tabcontent" style="border: 5px solid red; padding: 10px; margin-left: 15px;">Be the first to answer</div>';
-}
-?></p>
-<br>
-</div>
-<script>
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
     }
-  });
+} else {
+
+    echo "<br><br>Be the first to give answer";
 }
-</script>
-                        
-<?php
-  endwhile; ?>
+     
+endwhile;
+     ?><br><br>
 </body>
 </html>
