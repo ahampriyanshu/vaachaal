e930d81163d3f8fbeb64ced14c3edec032bc00b2
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-09 06:10:52 +0530

    Update postque.php

--- postque.php
+++ postque.php
@@ -1,9 +1,7 @@
 <?php  session_start(); ?>
 <?php
-
 include 'essentials/database.php';
 
-$title = $_POST['title'];
 $content = $_POST['content'];
 $level = $_POST['level'];
 $tym = $_POST['tym'];
@@ -11,7 +9,7 @@ $branch = $_POST['branch'];
 $username = $_SESSION["loggedin"];
 $date = date('m/d/Y h:i:s', time());
 
-$sql = "INSERT INTO questions(title,content,level,tym,branch,username,datetym) VALUES ('$title','$content','$level','$tym','$branch','$username','$date')";
+$sql = "INSERT INTO questions(content,level,tym,branch,username,datetym) VALUES ('$content','$level','$tym','$branch','$username','$date')";
 
 if ($con->query($sql) === TRUE) 
 {
