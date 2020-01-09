b9a47123d75be703eceecd1ce0b537618d3ef5e2
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-09 06:16:19 +0530

    Delete session.php

--- session.php
+++
@@ -1,8 +0,0 @@
-<?php session_start(); ?>
-<?php include("essentials/database.php"); ?>
-<?php // include("essentials/function.php"); ?>
-<?php
-		$_SESSION['user']=$username;
-		header('location:add.php');
-
-?>
