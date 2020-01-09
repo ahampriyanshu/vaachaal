9e1c362d095a54bf8ace6cbb8c1714d52e8fcd7c
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-09 22:47:53 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,5 +1,19 @@
-<style type="text/css">
-<!--
+<?php  session_start(); ?>
+<?php 
+    if(!isset($_SESSION['loggedin'])){
+    header('location:index.php');}
+?>
+<?php 
+  include("header.php");
+  include("panel.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Add Question</title>
+<link href="forum.css" rel="stylesheet" type="text/css">
+<style>
+
 .abt { 
     background: #fff;
     border: 1px solid #e2e2e2;
@@ -14,46 +28,21 @@
     overflow-y: hidden;
     top: 10%;
 }
-
-.heading{
-  font-family: 'Courier', sans-serif;
-}
-
-.sub{
-  font-family: 'Trebuchet MS', sans-serif;
-}
-
--->
 </style>
-<?php
-include("header.php");
-
-if (isset($_SESSION['login']))
-{
-echo "<div class=\"abt\" style=\"position: absolute; top:12%;left:20%;\">
-  <img class=\"img-circle\" style=\"position: absolute; top:1%;left:5%;\" src=\"IIS.jpg\"  width=\"200px\" height=\"150px\" />
+</head>
+<body background="img/back.jpg">
+  <?php
+  echo "<div class=\"abt\" style=\"position: absolute; top:12%;left:20%;\">
+  <img class=\"img-circle\" style=\"position: absolute; top:1%;left:5%;\" src=\"img/IIS.jpg\"  width=\"200px\" height=\"150px\" />
     </center>
-    <img class=\"img-circle\" style=\"position: absolute; top:3%;left:38%;\" src=\"IITD.png\"  width=\"170px\" height=\"130px\" />
+    <img class=\"img-circle\" style=\"position: absolute; top:3%;left:38%;\" src=\"img/IITD.png\"  width=\"170px\" height=\"130px\" />
     </center>
-    <img class=\"img-circle\" style=\"position: absolute; top:3%;right:8%;\" src=\"IITM.png\"  width=\"160px\" height=\"130px\" />
+    <img class=\"img-circle\" style=\"position: absolute; top:3%;right:8%;\" src=\"img/IITM.png\"  width=\"160px\" height=\"130px\" />
     </center>
   <h1 style=\"position:absolute;top:26%;left:10%;\" class=\"heading\"><center>GRADUATE APTITUDE TEST IN ENGINEERING</center></h1>
-  <h style=\"position:absolute;top:38%;left: 3%;right: 3%;\" class=\"sub\">GATE is a computer-based exam conducted at the national level with an aim to examine the understanding of various Engineering and Science UG subjects.GATE exam consists of 65 MCQs and numerical question over a 3 hour duration.GATE 2020 is being conducted by IIT-D,IIT-M IISc.GATE score is valid for 3 years and enables students to gain admission to various PG programs such as ME,BE and PhD in IITs,IISc and several other prestigious universities.Top rank holders also get direct interview calls for prestigious government jobs in PSUs</h>
-  <img class=\"img-circle\" style=\"position: absolute; bottom:2%;left:5%;\" src=\"PSU.png\"  width=\"750px\" height=\"220px\" />
+  <p style=\"position:absolute;top:38%;left: 3%;right: 3%;\" class=\"sub\">GATE is a computer-based exam conducted at the national level with an aim to examine the understanding of various Engineering and Science UG subjects.GATE exam consists of 65 MCQs and numerical question over a 3 hour duration.GATE 2020 is being conducted by IIT-D,IIT-M IISc.GATE score is valid for 3 years and enables students to gain admission to various PG programs such as ME,BE and PhD in IITs,IISc and several other prestigious universities.Top rank holders also get direct interview calls for prestigious government jobs in PSUs</p>
+  <img class=\"img-circle\" style=\"position: absolute; bottom:2%;left:5%;\" src=\"img/PSU.png\"  width=\"750px\" height=\"220px\" />
 </div>";
-}
-else {
-  echo "<div class=\"abt\" style=\"position: absolute; top:12%;left:5%;\">
-  <img class=\"img-circle\" style=\"position: absolute; top:1%;left:5%;\" src=\"IIS.jpg\"  width=\"200px\" height=\"150px\" />
-    </center>
-    <img class=\"img-circle\" style=\"position: absolute; top:3%;left:38%;\" src=\"IITD.png\"  width=\"170px\" height=\"130px\" />
-    </center>
-    <img class=\"img-circle\" style=\"position: absolute; top:3%;right:8%;\" src=\"IITM.png\"  width=\"160px\" height=\"130px\" />
-    </center>
-  <h1 style=\"position:absolute;top:26%;left:10%;\" class=\"heading\"><center>GRADUATE APTITUDE TEST IN ENGINEERING</center></h1>
-  <h style=\"position:absolute;top:38%;left: 3%;right: 3%;\" class=\"sub\">GATE is a computer-based exam conducted at the national level with an aim to examine the understanding of various Engineering and Science UG subjects.GATE exam consists of 65 MCQs and numerical question over a 3 hour duration.GATE 2020 is being conducted by IIT-D,IIT-M IISc.GATE score is valid for 3 years and enables students to gain admission to various PG programs such as ME,BE and PhD in IITs,IISc and several other prestigious universities.Top rank holders also get direct interview calls for prestigious government jobs in PSUs</h>
-  <img class=\"img-circle\" style=\"position: absolute; bottom:2%;left:5%;\" src=\"PSU.png\"  width=\"750px\" height=\"220px\" />
-</div>";
-}
-?>
-</script> 
+?>  
+</body>
+</html>
