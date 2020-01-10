04570bb4034583c297c648953ad6516e5d2904df
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-10 23:44:45 +0530

    Update signupuser.php

--- signupuser.php
+++ signupuser.php
@@ -1,8 +1,8 @@
-<?php 
-	include("header.php");
-	include("essentials/database.php"); ?>
 <?php
-
+	include("header.php");
+include("essentials/database.php"); ?>
+<?php
+date_default_timezone_set('Asia/Kolkata');
 $newuser = $_POST['username'];
 $pass = $_POST['pass'];
 $name  = $_POST['name'];
@@ -10,24 +10,23 @@ $secu  = $_POST['security'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 $date = date('m/d/Y h:i:s', time());
-
-$sql = "INSERT INTO userbase (username,password,name,security,phone,email,datetym) VALUES 
+$sql = "INSERT INTO userbase (username,password,name,security,phone,email,datetym) VALUES
 ('$newuser','$pass','$name','$secu','$phone','$email','$date')";
 $q = "select * from userbase where username = '$newuser'";
 $result = mysqli_query($con,$q);
 $num = mysqli_num_rows($result);
 if ($num == 1) {
-	echo "<script>
-    alert('Username already taken');
-    document.location='signup.php';
+echo "<script>
+alert('Username already taken');
+document.location='signup.php';
 </script>";
-} 
-else {
-	$qy = "INSERT INTO userbase(username,password,name,security,phone,email,datetym) VALUES ('$a$newuser','$pass','$name','$secu','$phone','$email','$date')";
-	mysqli_query($con,$qy);
-	echo "<script>
-    alert('Login ID successfully created');
-document.location='index.php';
-</script>";	 
 }
-?>	
+else {
+$qy = "INSERT INTO userbase(username,password,name,security,phone,email,datetym) VALUES ('$a$newuser','$pass','$name','$secu','$phone','$email','$date')";
+mysqli_query($con,$qy);
+echo "<script>
+alert('Login ID successfully created');
+document.location='index.php';
+</script>";
+}
+?>
