10676af34e8981f593e1d1e478121f3d47cf999b
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-11 04:45:58 +0530

    Add files via upload

---
+++ admindeleteans.php
@@ -0,0 +1,107 @@
+<?php  session_start(); ?>
+<?php
+if(!isset($_SESSION['loggedin'])){
+header('location:index.php');}
+?>
+<?php
+include("header.php");
+include("panel.php");
+include("essentials/database.php");
+?>
+<!DOCTYPE html>
+<html>
+  <link href="forum.css" rel="stylesheet" type="text/css">
+  <head>
+    <title>Delete Answer</title>
+  </head>
+  <style type="text/css">
+  .question_box {
+  background: #f1f1f1;
+  border: 2px solid #e2e2e2;
+  box-shadow: 0 0 5px #888;
+  border-radius: 5px;
+  width: 70%;
+  padding: 40px;
+  position: relative;
+  bottom: initial;
+  margin: auto;
+  overflow-y: hidden;
+  }
+  #title{
+  line-height: 1.5;
+  color: #333;
+  tab-size: 4;
+  word-break: break-word;
+  text-align: left;
+  direction: ltr;
+  user-select: text;
+  font-size: 25px;
+  font-family: Courier new ;
+  }
+  #anstitle{
+  line-height: 1.5;
+  color: black;
+  tab-size: 4;
+  word-break: break-word;
+  text-align: left;
+  direction: ltr;
+  user-select: text;
+  font-size: 15px;
+  font-family: Courier new ;
+  }
+  #specs{
+  font-size:12px;
+  font-family: courier new ;
+  font-weight: bold;
+  color: #833AB4;
+  }
+  #line{
+  border: 1px solid red;
+  border-radius: 2px;
+  }
+  #details{
+  font-weight: bold;
+  color: red;
+  font-size:12px;
+  }
+  #answer_button {
+  position:relative;
+  left: 40%;
+  background-color:#833AB4;
+  color: white;
+  padding-bottom: 3px;
+  padding: 11px;
+  font-size: 11px;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  font-weight: bold;
+  font-family:'Trebuchet MS', sans-serif;
+  border-radius: 5%;
+  }
+  #answer_button:hover, #answer_button:focus {
+  background-color: #DB4437;
+  }
+  </style>
+  <body background="img/back.jpg">
+    <?php
+    $USR = $_SESSION['loggedin'];
+    $sql = "SELECT * FROM answers WHERE username = '$USR' ORDER BY datetym DESC ";
+    $result = $con->query($sql);
+    if ($result->num_rows > 0)
+    while($row = $result->fetch_assoc()) :?>
+    <div class="question_box" style="padding-left: 30px;">
+      <span id="title"><?php echo $row["content"]; ?></span><br><hr id="line"><br>
+      <span id="specs">time required is</span> &nbsp;<span id="details"><?php echo $row["tym"]; ?></span> &emsp;
+      <span id="specs">difficulty level according to user is</span>&nbsp;&nbsp;<span id="details"><?php echo $row["level"]; ?></span> &emsp;
+      <span id="specs">posted on</span> &nbsp;<span id="details"><?php echo $row["datetym"]; ?></span><br>
+      <form method="post" action="delansbyuser.php"><br>
+        <input  type="submit"  id="answer_button" value="Delete Answer"/>
+        <input type="hidden" name="aid" value="<?php echo $row['aid']; ?>"/>
+      </form>
+    </div><br><br>
+    <?php
+    endwhile;
+    ?>
+  </body>
+</html>
