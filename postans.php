461b6508846848e9ebe606b1682440e0ae5c0d9f
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-10 23:45:35 +0530

    Update postans.php

--- postans.php
+++ postans.php
@@ -1,22 +1,24 @@
 <?php  session_start(); ?>
 <?php
+if(!isset($_SESSION['loggedin'])){
+header('location:index.php');}
+?>
+<?php
 include 'essentials/database.php';
-
+date_default_timezone_set('Asia/Kolkata');
 $qid = $_POST['qid'];
 $content = $_POST['content'];
 $username = $_SESSION["loggedin"];
 $date = date('m/d/Y h:i:s', time());
 $tym = $_POST['tym'];
 $level = $_POST['level'];
-
 $sql = "INSERT INTO answers (id,content,username,datetym,tym,level) VALUES ('$qid','$content','$username','$date','$tym','$level')";
-
-if ( $con -> query ($sql) === TRUE) 
+if ( $con -> query ($sql) === TRUE)
 {
-    header('location:home.php');
-} 
-else 
+header('location:home.php');
+}
+else
 {
 	echo $username;
-}	
+	}
 ?>
