cac0ad7b79a8ee6d044ac69947a6866e835de486
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-11 23:28:55 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -21,32 +21,11 @@ header('location:home.php');}
     overflow-y: hidden;
     top: 10%;
     }
-    .heading{
-    font-family: 'Courier', sans-serif;
-    }
-    .sub{
-    font-family: 'Trebuchet MS', sans-serif;
-    }
+.sub{
+  font-family: 'Trebuchet MS', sans-serif;
+}
     </style>
     <title>Index</title>
-    <script language="javascript">
-    function check()
-    {
-    if(document.login_form.username.value=="")
-    {
-    alert("Plese Enter Login Id");
-    document.login_form.username.focus();
-    return false;
-    }
-    
-    if(document.login_form.password.value=="")
-    {
-    alert("Plese Enter Your Password");
-    document.login_form.password.focus();
-    return false;
-    }
-    }
-    </script>
     <link href="forum.css" rel="stylesheet" type="text/css">
   </head>
   <body background="img/back.jpg">
@@ -67,14 +46,14 @@ header('location:home.php');}
     }
     }
     echo "<div class=\"abt\" style=\"position: absolute; top:12%;left:5%;\">
-      <img class=\"img-circle\" style=\"position: absolute; top:1%;left:5%;\" src=\"img/IIS.jpg\"  width=\"200px\" height=\"150px\" />
+      <img class=\"img-circle\" style=\"position: absolute; top:2%;left:8%;\" src=\"img/IIT_B.png\"  width=\"170px\" height=\"130px\" />
       </center>
-      <img class=\"img-circle\" style=\"position: absolute; top:3%;left:38%;\" src=\"img/IITD.png\"  width=\"170px\" height=\"130px\" />
+      <img class=\"img-circle\" style=\"position: absolute; top:3%;left:40%;\" src=\"img/IITD.png\"  width=\"170px\" height=\"130px\" />
       </center>
       <img class=\"img-circle\" style=\"position: absolute; top:3%;right:8%;\" src=\"img/IITM.png\"  width=\"160px\" height=\"130px\" />
       </center>
-      <h1 style=\"position:absolute;top:26%;left:10%;\" class=\"heading\"><center>GRADUATE APTITUDE TEST IN ENGINEERING</center></h1>
-      <h style=\"position:absolute;top:38%;left: 3%;right: 3%;\" class=\"sub\">GATE is a computer-based exam conducted at the national level with an aim to examine the understanding of various Engineering and Science UG subjects.GATE exam consists of 65 MCQs and numerical question over a 3 hour duration.GATE 2020 is being conducted by IIT-D,IIT-M IISc.GATE score is valid for 3 years and enables students to gain admission to various PG programs such as ME,BE and PhD in IITs,IISc and several other prestigious universities.Top rank holders also get direct interview calls for prestigious government jobs in PSUs</h>
+      <h1 style=\"position:absolute;top:26%; color:#333; left:9%;font-family:courier;\" class=\"heading\"><center>GRADUATE APTITUDE TEST IN ENGINEERING</center></h1>
+      <h style=\"position:absolute;top:38%; left: 3%;right: 3%;\" class=\"sub\">GATE is a computer-based exam conducted at the national level with an aim to examine the understanding of various Engineering and Science UG subjects.GATE exam consists of 65 MCQs and numerical question over a 3 hour duration.GATE 2020 is being conducted by IIT-D in Febuary.GATE score is valid for 3 years and enables students to gain admission to various PG programs such as ME,BE and PhD in IITs,IISc and several other prestigious universities.Top rank holders also get direct interview calls for prestigious government jobs in PSUs</h>
       <img class=\"img-circle\" style=\"position: absolute; bottom:2%;left:5%;\" src=\"img/PSU.png\"  width=\"750px\" height=\"220px\" />
     </div>";
     ?>
@@ -83,15 +62,15 @@ header('location:home.php');}
         <form method="post" name="login_form" action="" onSubmit="return check();">
           <center><img class="logocircle" src="img/1.png"  title="logo" width="210px" height="200px" border="1" /></center>
           <tr>
-            <th><input class="login_text_box" type="TEXT" title="enter your regitered LOGIN ID"  placeholder="LOGIN ID"  maxlength="20" size="25"   name="username"/></th>
+            <th><input class="login_text_box" type="TEXT" title="enter your regitered LOGIN ID"  placeholder="LOGIN ID"  maxlength="20" size="25"   name="username" required /></th>
           </tr>
           <tr>
-            <th><input class="login_text_box" type="password"  placeholder="ENTER PASSWORD" name="password" /></th>
+            <th><input class="login_text_box" type="password"  placeholder="ENTER PASSWORD" name="password"  required /></th>
           </tr>
           <?php
           if(isset($found))
           {
-          echo '<p class="inva" style="font"><center>Invalid Username or password</center></p>';
+          echo '<span class="inva" style="color:red;"><center>Invalid Username or password</center></span>';
           }
           ?>
           <tr>
@@ -102,10 +81,10 @@ header('location:home.php');}
       </tr>
       <tr>
         <td>&nbsp;&emsp;&emsp;&emsp;&emsp;
-          <button class="submit2" onclick="window.location.href = 'signup.php';" >New User?</button>
+          <button  class="submit2" onclick="window.location.href = 'signup.php';" >New User?</button>
         </td>
       </tr>
     </table>
-  </div>
+  </div >
 </body>
 </html>
