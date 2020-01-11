8e858b6b5a919c6f2832166e1917879ada153a8e
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-11 23:35:25 +0530

    Add files via upload

--- essentials/database.php
+++ essentials/database.php
@@ -1,17 +1,10 @@
-	<?php  
+<?php  	
 
 $host="localhost"; // Host name 
 $user="root";  // Mysql user 
-$db="forum";  // Database name 
+$db="GATE";  // Database name 
 
 $con=mysqli_connect("$host","$user","","$db") or die('Database not connected'); //connecting database
 
-if (!mysqli_select_db($con,$db)){    // Create database if not already there
-    $sql = "CREATE DATABASE ".$db;
-    if ($con->query($sql) === TRUE) {
-        echo "Database created successfully";
-    }else {
-        echo "Error creating database: " . $con->error;
-    }
-} 
+
 ?>
