aed6fdc49697d60481bd322f1b5c53ca1399265c
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-16 23:04:41 +0530

    Add files via upload

--- home.php
+++ home.php
@@ -1,166 +1,185 @@
-<?php session_start(); ?>
 <?php
-if(!isset($_SESSION['loggedin'])){
-header('location:index.php');}
-?>
-<?php
-include("essentials/script.php");
-include("header.php");
-include("essentials/database.php");
-include("panel.php");
-?>
+if(isset($_SESSION['loggedin'])){
+header('location:home.php');}
+?><?php include("header.php"); ?>
+<?php include("essentials/database.php"); ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <meta name="description" content="GNDEC GATE FORUM">
-  <meta name="keywords" content="gate,priyanshumay,gne,gndec,">
-  <meta name="author" content="PriyanshuMay,priyanshumay">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home</title>
-        <link href="forum.css" rel="stylesheet" type="text/css">
-        <style type="text/css">
-        body{
-        background-color: ;
-        padding-left: 200px;
-        }
-        .collapsible {
-        background-color: white;
-        cursor: pointer;
-        width: 100%;
-        padding: 50px;
-        border: none;
-        border-radius: 4px;
-        text-align: left;
-        font-size: 15px;
-        }
-        .active, .collapsible:hover {
-        background-color: #e6ffff;
-        font-size: 110%;
-        color: red;
-        }
-        .content {
-        padding: 0 18px;
-        display: none;
-        overflow: auto;
-        background-color:;
-        }
-        #line{
-        border: 1px solid red;
-        border-radius: 2px;
-        }
-        #answer_button {
-        position:relative;
-        left: 40%;
-        background-color:#833AB4;
-        color: white;
-        padding-bottom: 3px;
-        padding: 11px;
-        font-size: 11px;
-        border: none;
-        font-weight: bold;
-        cursor: pointer;
-        font-weight: light;
-        font-family:'Trebuchet MS', sans-serif;
-        border-radius: 5%;
-        }
-        #answer_button:hover, #answer_button:focus {
-        background-color: #DB4437;
-        }
-        #title{
-        line-height: 1.5;
-        color: #333;
-        tab-size: 4;
-        word-break: break-word;
-        text-align: left;
-        direction: ltr;
-        user-select: text;
-        font-size: 25px;
-        font-family: Courier new ;
-        }
-        #anstitle{
-        line-height: 1.5;
-        color: black;
-        tab-size: 4;
-        word-break: break-word;
-        text-align: left;
-        direction: ltr;
-        user-select: text;
-        font-size: 15px;
-        font-family: Courier new ;
-        }
-        #specs{
-        font-size:12px;
-        font-family: courier new ;
-        font-weight: bold;
-        color: #833AB4;
-        }
-        #details{
-        font-weight: bold;
-        color: red;
-        font-size:12px;
-        }
-        #answer_box{
-        padding:10px;
-        padding-bottom: 20px;
-        box-shadow: 0 0 5px #888;
-        background-color: #d6d6c2;
-        width: 97%;
-        height: auto;
-        border: 2px solid #d6d6c2;
-        }
-        #answer_box:hover {
-        background-color: #e0e0d1;
-        }
-        </style>
-    </head>
-    <body >
+  <head>
+    <meta name="description" content="GNDEC GATE FORUM">
+    <meta name="keywords" content="gate,priyanshumay,gne,gndec,">
+    <meta name="author" content="PriyanshuMay,priyanshumay">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    html,body{
+    margin: 0;
+    padding: 0;
+    }
+    body{
+    height: 100%;
+    width: 100%;
+    }
+    
+    .abt_index {
+    background: #fff;
+    border: 1px solid #e2e2e2;
+    box-shadow: 0 0 5px #888;
+    border-radius: 4px;
+    padding-top: 25px;
+    width: 850px;
+    height: 500px;
+    position: absolute;
+    overflow: hidden;
+    top: 9%;
+    background-repeat: no-repeat;
+    background-size: contain;
+    }
+    .tech_used:hover{
+    transform: scale(1.2);
+    }
+    #bulb_index:hover{
+    transform: scale(1.01);
+    }
+    #signinbox_index_web {
+    background: #fff;
+    border: 1px solid #e2e2e2;
+    box-shadow: 0 0 5px #888;
+    border-radius: 4px;
+    padding-top: 2px;
+    width: 280px;
+    position: absolute;
+    }
+    #signinbox_index_mob {
+    background: #fff;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: none;
+    }
+    .inva{
+    font-family: courier new;
+    font-weight: bold;
+    color:#333;
+    }
+    .login_text_index {
+    width: 100%;
+    font-size: 15px;
+    line-height: 1.4;
+    padding-left: 8px;
+    padding-right: 8px;
+    min-height: 32px;
+    margin-bottom: 8px;
+    border: 1px solid #e2e2e2;
+    box-shadow: 0 0 5px #888;
+    border-radius: 4px;
+    }
+    </style>
+    <title>Index</title>
+    <link href="forum.css" rel="stylesheet" type="text/css">
+  </head>
+  <body background="img/back.jpg">
+    <?php
+    extract($_POST);
+    if(isset($submit))
+    {
+    $rs=mysqli_query($con,"select * from userbase where username='$username' and password='$password'");
+    if(mysqli_num_rows($rs)<1)
+    {
+    $found="N";
+    }
+    else
+    {
+    session_start();
+    $_SESSION["loggedin"] = $username ;
+    header('location:home.php');
+    }
+    }
+    echo '';
+    ?>
+    <div class="abt_index" id ="abt_visibility" style="position: absolute; top:12%;left:5%; ">
+      <img  id="bulb_index" src="img/bulb_on.png"   alt="logo"  width="500px"   height="500px;"
+      onmouseover="this.src='img/bulb_on.png';"  onmouseout="this.src='img/bulb_off.png';" style="position:relative; top:-11%; left:21% ;  border="" />
+      <img class=' tech_used ' src='img/php.png'  style="position:absolute; left:13% ; bottom:3%;" title="php" width="75px" height="60px" border="" />
+      <img class=" tech_used" src="img/css.png"  style="position:absolute; left:32% ; bottom:3%; " title="CSS3" width="50px" height="60px" border="" />
+      <img class=" tech_used" src="img/mysql.png"  style="position:absolute; left:47% ; bottom:3%; " title="mysql" width="60px" height="60px" border="" />
+      <img class=" tech_used" src="img/html.png"  style="position:absolute; left:62% ; bottom:3%; " title="HTML5" width="60px" height="60px" border="" />
+      <img class=" tech_used" src="img/js.png"  style="position:absolute; left:77% ; bottom:3%; " title="JS" width="60px" height="60px" border="" />
+    </div>
+    <div id="signinbox_index_web" style="position: absolute; top:16%;right:5%;">
+      <table align="center" border="0" WIDTH="90%" height="250">
+        <form method="post" name="login_form" action="" onSubmit="return check();">
+          <center><img class="logocircle" src="img/1.png"  title="logo" width="210px" height="200px" border="" /></center>
+          <tr>
+            <th><input class="login_text_index" type="text" title="enter your regitered LOGIN ID"  placeholder="LOGIN ID"  maxlength="20" size="25"   name="username" required /></th>
+          </tr>
+          <tr>
+            <th><input class="login_text_index" type="password"  placeholder="ENTER PASSWORD" name="password"  required /></th>
+          </tr>
+          <?php
+          if(isset($found))
+          {
+          echo '<span class="inva" style=""><center>Invalid Username or password</center></span>';
+          }
+          ?>
+          <tr>
+            <td>&emsp;&emsp;<input class="submit" type="submit" name="submit" Value="Login"/>
+          &emsp;&emsp;</form>
+          <button class="submit2" onclick="window.location.href = 'forgot.php';" >Forgot Password?</button>
+        </td>
+      </tr>
+      <tr>
+        <td>&nbsp;&emsp;&emsp;&emsp;&emsp;
+          <button  class="submit2" onclick="window.location.href = 'signup.php';" >New User?</button>
+        </td>
+      </tr>
+    </table>
+  </div >
+  <div id="signinbox_index_mob" >
+    <table align="center" border="0"  width ="75%" height="50%">
+      <form method="post" name="login_form" action="" onSubmit="return check();">
+        <center><img class="logocircle" src="img/1.png"  title="logo" width="210px" height="200px" border="" /></center>
+        <tr>
+          <th><input class="login_text_index" type="TEXT" title="enter your regitered LOGIN ID"  placeholder="LOGIN ID"  maxlength="20" size="25"   name="username" required /></th>
+        </tr>
+        <tr>
+          <th><input class="login_text_index" type="password"  placeholder="ENTER PASSWORD" name="password"  required /></th>
+        </tr>
         <?php
-        $sql = "SELECT id,content,level,tym,branch,username,datetym FROM questions ORDER BY datetym DESC";
-        $result = $con->query($sql);
-        if ($result->num_rows > 0)
-        while($row = $result->fetch_assoc()) :?>
-        <button type="button" class="collapsible">
-        <span id="title"><?php echo $row["content"]; ?></span><br><hr id="line"><br>
-        <span id="specs">Asked by </span>&nbsp;<span id="details"><?php echo $row["username"]; ?></span> &emsp;
-        <span id="specs">time alloted is</span> &nbsp;<span id="details"><?php echo $row["tym"]; ?></span> &emsp;
-        <span id="specs">difficulty level estimated is</span>&nbsp;&nbsp;<span id="details"><?php echo $row["level"]; ?></span> &emsp;
-        <span id="specs">question comes under</span> &nbsp;<span id="details"><?php echo $row["branch"]; ?></span><span id="specs"> branch</span>&emsp;
-        <span id="specs">posted on</span> &nbsp;<span id="details"><?php echo $row["datetym"]; ?></span><br>
-        <form method="post" action="addans.php"><br>
-            <input  type="submit"id="answer_button" value="Have a better answer?"/>
-            <input type="hidden" name="id" value="<?php echo $row['id']; ?>"/>
-        </form>
-        </button>
-        <?php
-        echo '<div class="content">';
-            $queid = $row["id"];
-            $ql = "SELECT * FROM answers WHERE id = '$queid' ORDER BY datetym DESC ";
-            $resul = $con->query($ql);
-            if ($resul->num_rows > 0) {
-            while($ro = $resul->fetch_assoc()) {
-            echo "<br><div id='answer_box'>
-                <span id='anstitle'><br> ". $ro["content"] . "</span><br><span id='specs'> <br>&emsp;&emsp;&emsp;Time required is </span>&nbsp;<span id='details'> " . $ro ["tym"] . "&emsp;&emsp;<span id='specs'> Difficulty Level according to user is </span>&nbsp; ". $ro["level"] ."&emsp;&emsp;<span id='specs'> Answered by </span>&nbsp; " . $ro["username"] . "&emsp;&emsp;<span id='specs'> answered on </span>&nbsp; " . $ro["datetym"] ."</span></div>";
-                }
-                } else {
-                echo "<br><div id='answer_box'><span id='anstitle'>Be the first to answer</span></div>";
-                }
-            echo '<br><br></div>';
-            endwhile; ?>
-            <script>
-            var coll = document.getElementsByClassName("collapsible");
-            var i;
-            for (i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-            content.style.display = "none";
-            } else {
-            content.style.display = "block";
-            }
-            });
-            }
-            </script>
-            <br><br><br><br>
-        </body>
-    </html>
+        if(isset($found))
+        {
+        echo '<span class="inva" style=""><center>Invalid Username or password</center></span>';
+        }
+        ?>
+        <tr>
+          <td>&emsp;<input class="submit" type="submit" name="submit" Value="Login"/>
+        &emsp;</form>
+        <button class="submit2" onclick="window.location.href = 'signup.php';" >New User ?</button>
+      </td>
+    </tr>
+    <tr>
+      <td>&emsp;&emsp;
+        <button  class="submit2" onclick="window.location.href = 'forgot.php';" >Forgot password</button>
+      </td>
+    </tr>
+  </table>
+</div >
+<script>
+function myFunction(y) {
+if (y.matches) {
+document.getElementById("abt_visibility").style.display = "none";
+document.getElementById("signinbox_index_web").style.display = "none";
+document.getElementById("signinbox_index_mob").style.display = "block";
+document.body.style.background = "none";
+}else{
+document.getElementById("signinbox_index_web").style.display = "block";
+document.getElementById("signinbox_index_mob").style.display = "none";
+document.getElementById("abt_visibility").style.display = "block";
+document.body.style.background = "url(img/back.jpg)";
+}
+}
+var y = window.matchMedia("(max-width: 420px)")
+myFunction(y)
+y.addListener(myFunction)
+</script>
+</body>
+</html>
