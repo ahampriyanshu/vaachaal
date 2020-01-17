f5adc4a6069d4e7046d4b9e1840e2fac1355674a
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-17 22:32:54 +0530

    Adding comments

--- essentials/database.php
+++ essentials/database.php
@@ -1,14 +1,15 @@
 <?php  	
 
 $host="localhost"; // Host name 
-$user="root";  // Mysql user 
+$user="root";     // Mysql user 
+                 // User can here declare a variable for a password 
 
-$con=mysqli_connect("$host","$user","") or die('Database not connected'); //connecting database
+$con=mysqli_connect("$host","$user","") or die('Database not connected'); //connecting database without a password
 
 $db = mysqli_select_db($con,"forum");
-if (empty($db)) 
+if (empty($db))     // if database doesn't exist 
 {
-	$dbcr = 'create database forum';
+	$dbcr = 'create database forum';   // create a database `forum` 
 	$check = mysqli_query($con,$dbcr);
 
 
@@ -23,9 +24,9 @@ if (!$check) {
   $table1 = " select * from userbase";
   $check1 = mysqli_query($con,$table1);
 
-  if (!$check1) {
+  if (!$check1) {                        // if table userbase doesn't exist
 
-  	$create1 = "CREATE TABLE userbase (
+  	$create1 = "CREATE TABLE userbase (      /* create table userbase */
     user_id int(50) NOT NULL AUTO_INCREMENT,
     username varchar(50) NOT NULL,
     password varchar(50) NOT NULL,
@@ -45,11 +46,11 @@ if (!$check) {
 $table2 = " select * from questions";
   $check2 = mysqli_query($con,$table2);
   
-  if (!$check2) {
+  if (!$check2) {                         // same logic as above
 
     $create2 = "CREATE TABLE questions (
     id   int(50) NOT NULL AUTO_INCREMENT,
-    content   text(10000) NOT NULL,
+    content   varchar(10000) NOT NULL,
     level     varchar(50) NOT NULL,
     tym       varchar(50) NOT NULL,
     branch    varchar(50) NOT NULL,
@@ -66,12 +67,12 @@ $table2 = " select * from questions";
 $table3 = " select * from answers";
   $check3 = mysqli_query($con,$table3);
   
-  if (!$check3) {
+  if (!$check3) {                           // same logic as above
 
     $create3 = "CREATE TABLE answers (
     aid   int(50) NOT NULL AUTO_INCREMENT,
     id    int(50) NOT NULL,
-    content    text(10000) NOT NULL,
+    content    varchar(10000) NOT NULL,
     username   varchar(50) NOT NULL,
     datetym    varchar(50) NOT NULL,
     tym        varchar(50) NOT NULL,
@@ -87,7 +88,7 @@ $table3 = " select * from answers";
 $table4 = " select * from admin";
   $check4 = mysqli_query($con,$table4);
   
-  if (!$check4) {
+  if (!$check4) {                               // once again
 
     $create4 = "CREATE TABLE admin (
     login_id varchar(50) NOT NULL,
@@ -105,9 +106,9 @@ $admin_check_result = mysqli_query($con, $admin_check_query);
 
 $admin_check_row = mysqli_fetch_array($admin_check_result, MYSQLI_ASSOC);
 
-if(! $admin_check_row) {
+if(! $admin_check_row) {        //entering data for admin
 
-  $admin_info = "INSERT INTO admin (login_id, password)
+  $admin_info = "INSERT INTO admin (login_id, password)  /* leaving superpassword empty */
   VALUES ('gne', 'gate')";
   
   $check5 = mysqli_query($con,$admin_info);
@@ -115,4 +116,5 @@ if(! $admin_check_row) {
     echo 'admin not created';
   }
 }
+
 ?>
