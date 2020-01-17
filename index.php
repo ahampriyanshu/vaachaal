09ba5eaa24b93e55b6ad512cc6b2b67eefb3d5e9
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-17 19:17:41 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -142,21 +142,6 @@ include("panel.php");
                 }
             echo '<br><br></div>';
             endwhile; ?>
-            <script>
-            var coll = document.getElementsByClassName("collapsible");
-            var i;
-            for (i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-            content.style.display = "none";
-            } else {
-            content.style.display = "block";
-            }
-            });
-            }
-            </script>
             <br><br><br><br>
         </body>
     </html>
